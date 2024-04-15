29f500dacc76a946764ffd05f903b41fdcca1025
brammdamm <brammdamm@gmail.com>
2024-04-14 21:29:54 -0700

    adding files

--- src/test/java/org/example/LoginPage.java
+++ src/test/java/org/example/LoginPage.java
@@ -1,26 +1,26 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 
-public class HomePage {
+public class LoginPage {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.gecko.driver", "/Users/birendrabishwakarma/Desktop/geckodriver");
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.amazon.com/");
         Thread.sleep(5000);
 
-        String homePageTitleName = driver.getTitle();
-        System.out.println("home page is" + homePageTitleName);
-        Assert.assertEquals("Amazon.com. Spend less. Smile more.", homePageTitleName);
-        }
+        WebElement SignInButton = driver.findElement(By.cssSelector("a#nav-link-accountList.nav-a.nav-a-2.nav-progressive-attribute"));
+        Thread.sleep(5000);
+        SignInButton.click();
+        String title2 = driver.getTitle();
+        // System.out.println(title2);
+        Assert.assertEquals("Amazon Sign-In", title2);
+
+
+
+    }
 }
-
-
-
-
-
-
-
-
